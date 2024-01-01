a6e2d7dcc54156c33137667e885784d4f958aa99
MUSITELLI ALESSANDRO <alessandro.musitelli@intesasanpaolo.com>
2024-01-02 00:21:42 +0100

    init fixed-width file reader

---
+++ ibis-snippets/main.py
@@ -0,0 +1,7 @@
+import ibis
+from ibis import _ as d_
+from ibis import config, examples, ir
+from ibis.expr import datashape as ds
+from ibis.expr import datatypes as dt
+
+print(f"ibis version: {ibis.__version__}")
